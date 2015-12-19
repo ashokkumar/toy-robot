97cb6584682bad8fae0c620bdfa30e86d3af5326
Ashok <ashok.contact@gmail.com>
2015-12-20 02:14:53 +1100

    Refactored the table_top to make it more readable

--- lib/table_top.rb
+++ lib/table_top.rb
@@ -1,10 +1,12 @@
 class TableTop
-  def initialize(length, breath)
+  def initialize(length, breadth)
     @length = length
-    @breath = breath
+    @breadth = breadth
+    @lowest_length = 0
+    @lowest_breadth = 0
   end
 
   def has_coordinate?(coordinate)
-    coordinate.x <= @length && coordinate.y <= @breath && coordinate.x >= 0 &&  coordinate.y >= 0
+    coordinate.x.between?(@lowest_breadth, @length) && coordinate.y.between?(@lowest_breadth, @breadth)
   end
 end
